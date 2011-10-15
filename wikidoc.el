1c7a3e71b59005747285ad3fd7294e38c20cfaf8
Nic Ferrier <nic@ferrier.me.uk>
2011-10-15 16:28:42 +0100

    added translation of function args to the list of stuff done.

--- wikidoc.el
+++ wikidoc.el
@@ -5,7 +5,7 @@
 ;; Author: Nic Ferrier <nic@ferrier.me.uk>
 ;; Maintainer: Nic Ferrier <nic@ferrier.me.uk>
 ;; Created: 5th October 2010
-;; Version: 0.3
+;; Version: 0.4
 ;; Keywords: lisp
 
 ;; This file is NOT part of GNU Emacs.
@@ -56,6 +56,8 @@
 
 ;;; Code:
 
+(require 'ert)
+
 ;;;###autoload
 (defun wikidoc-grab-list (prefix &optional no-private)
   "Grab a list or functions matching PREFIX possibly with NO-PRIVATE"
@@ -73,21 +75,69 @@
      obarray)
     res))
 
-(defun wikidoc--convert-line (line)
+(defun wikidoc--convert-line (line &optional arguments-to-mangle)
   "Converts a single LINE of function documentation.
 
-This deals with things like quoted LISP forms which can be turned into links"
-  (save-match-data
-    (while (string-match ".*\\('[^']+'\\).*" line)
-      (setq line (replace-match 
-                  (format "[[%s]]" (let ((name (match-string 1 line)))
-                                     (save-match-data 
-                                       (string-match "'\\([^']+\\)'" name)
-                                       (match-string 1 name))))
-                  nil nil line 1))) line))
+This deals with things like quoted LISP forms which can be turned
+into links.
 
-(defun wikidoc--convert (str)
-  "Convert function documentation type doc STR to creole."
+ARGUMENTS-TO-MANGLE can be a list of arguments which are to be
+detected and replaced like:
+
+ARGUMENT -> //argument//
+
+The list should be of symbols, not strings."
+  (let ((arglstre (if arguments-to-mangle
+                      (concat "\\("
+                              (mapconcat
+                               (lambda (sym)
+                                 (upcase (symbol-name sym)))
+                               arguments-to-mangle
+                               "\\|")
+                              "\\)")
+                    nil)))
+    (save-match-data
+      (while (string-match ".*\\('[^']+'\\).*" line)
+        (setq line (replace-match 
+                    (format "[[%s]]" (let ((name (match-string 1 line)))
+                                       (save-match-data 
+                                         (string-match "'\\([^']+\\)'" name)
+                                         (match-string 1 name))))
+                    nil nil line 1)))
+      (if arglstre 
+          (replace-regexp-in-string 
+           arglstre 
+           (lambda (matched)
+             (format "//%s//" (downcase matched)))
+           line
+           't
+           nil)
+        line
+      ))))
+  
+(ert-deftest wikidoc-test-convert-line ()
+  "Can we convert lines with lisp refs and arguments?"
+  (let ((l '("This is a line of documentation with 'lisp-references'" .
+             "This is a line of documentation with [[lisp-references]]")))
+    (should (equal (cdr l)
+                   (wikidoc--convert-line (car l))))
+    )
+  (let ((l '("A line of documentation with 2 args: CAR and CDR" .
+             "A line of documentation with 2 args: //car// and //cdr//")))
+    (should (equal (cdr l)
+                   (wikidoc--convert-line (car l) '(car cdr))))
+    )
+  )
+
+(defun wikidoc--convert (str &optional arguments-to-mangle)
+  "Convert function documentation type doc STR to creole.
+
+ARGUMENTS-TO-MANGLE can be a list of arguments which are to be
+detected and replaced like:
+
+ARGUMENT -> //argument//
+
+The list should be of symbols, not strings."
   (let (in-pre)
     (concat 
      (mapconcat
@@ -98,12 +148,12 @@ This deals with things like quoted LISP forms which can be turned into links"
               line
             (progn 
               (setq in-pre 't)
-              (concat "{{{\n" (wikidoc--convert-line line)))))
+              (concat "{{{\n" (wikidoc--convert-line line arguments-to-mangle)))))
          ((and in-pre (not (string-match "^ " line)))
           (setq in-pre nil)         
-          (concat "}}}\n" (wikidoc--convert-line line)))
+          (concat "}}}\n" (wikidoc--convert-line line arguments-to-mangle)))
          ('t 
-          (wikidoc--convert-line line))))
+          (wikidoc--convert-line line arguments-to-mangle))))
       (split-string str "\n")
       "\n")
      ;; end any pre that we started
@@ -132,16 +182,29 @@ region is killed before the new wiki text is inserted.
   (let* ((lst (sort 
                (wikidoc-grab-list elisp-prefix 't)
                'string-lessp))
-         ;; We're relying on dynamic scope here to set buffer later
          (mapfn (lambda (fn)
-                  (with-current-buffer buffer
-                    (insert (format "=== %s %s ===\n\n%s\n\n\n" 
-                                    (symbol-name fn) 
-                                    ;; Could also capture the keywords here in some sort of hash
-                                    ;; so we can refer to them easily
-                                    (let ((args (help-function-arglist fn)))
-                                      (upcase (format "%s" args)))
-                                    (wikidoc--convert (documentation fn))))))))
+                  (with-current-buffer buffer ;; We're relying on dynamic scope here to set buffer later
+                    (insert 
+                     (let (arglist)
+                       (format 
+                        "=== %s %s ===\n\n%s\n\n\n" 
+                        (symbol-name fn) ;; the func name
+                        (let* ((args (help-function-arglist fn))) ;; the arglist
+                          (mapconcat
+                           (lambda (arg)
+                             (cond 
+                              ((or
+                                (equal '&optional arg)
+                                (equal '&rest arg))
+                               (format "{{{%s}}}" arg))
+                              ('t
+                               (add-to-list 'arglist arg)
+                               (format "//%s//" arg))))
+                           args
+                           " "))
+                        (let ((docbody (documentation fn)))  ;; the doc body
+                          (wikidoc--convert docbody arglist))
+                        )))))))
     (if (not (bufferp buffer))
         (progn
           (setq buffer (get-buffer-create (format "*wikidoc-%s*" elisp-prefix)))
